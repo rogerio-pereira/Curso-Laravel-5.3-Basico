125f00e0fc97b9021540b80cdcee6c6e07a690cf
Rogério Pereira <rogerio@colmeiatecnologia.com.br>
2016-12-23 15:34:18 -0200

    Aula 5 - Routes Final

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,64 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
-|
-*/
+//Rotas do tipo "/painel/*"
+Route::group(['prefix' => '/painel', 'middleware' => 'auth'], function()
+{
+    Route::get('/users', function()
+    {
+        return 'Controle de Users';
+    });
+
+    Route::get('/financeiro', function()
+    {
+        return 'Financeiro do Painel';
+    });
+
+    Route::get('/', function()
+    {
+        return 'Dashboard';
+    });
+});
+
+Route::get('/login', function(){
+    return '#form login';
+});
+
+Route::get('/categoria2/{$idCat?}', function($idCat=null){
+    return "Posts da categoria {$idCat}";
+});
+
+Route::get('/categoria/{$idCat}', function($idCat){
+    return "Posts da categoria {$idCat}";
+});
+
+Route::get('/nome/nome2/nome7', function(){
+    return 'Rota grande';
+})->name('rota.nomeada');
+
+Route::any('/any', function () {
+    return 'Route any';
+});
+
+Route::match(['get', 'post'], '/match', function () {
+    return 'Route match';
+});
+
+Route::post('/post', function () {
+    return 'Route Post';
+});
+
+Route::get('/contato', function () {
+    return 'Contato';
+});
+
+Route::any('/empresa', function () {
+    return view('empresa');
+});
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('rota.nomeada');
 });
+
+/*Route::get('/', function () {
+    return view('welcome');
+});*/
