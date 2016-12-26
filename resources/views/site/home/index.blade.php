78fd1ad0c6a2eef81d68378d74bf62247ab37b10
Rogério Pereira <rogerio@colmeiatecnologia.com.br>
2016-12-26 17:21:53 -0200

    Aula 11 - Blade @stack

--- resources/views/site/home/index.blade.php
+++ resources/views/site/home/index.blade.php
@@ -60,3 +60,9 @@
 
     @include('site.include.sidebar', compact('var1'));
 @endsection
+
+{{--Arquivo scripts--}}
+@push('scripts')
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+@endpush
--- resources/views/site/template/template1.blade.php
+++ resources/views/site/template/template1.blade.php
@@ -2,6 +2,9 @@
 <html>
     <head>
         <title>{{$title or 'Curso de Laravel 5.3'}}</title>
+
+        {{--Pode ser incluido dentro do body tbm... Está sendo chamado no home--}}
+        @stack('scripts');
     </head>
 
     <body>
